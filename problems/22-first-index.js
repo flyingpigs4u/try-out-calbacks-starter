d7614c2da7c90765d8a5bde4e67b8ceb99f53ca8
Emily <elicholai@scratch.fi>
2022-08-31 17:22:46 -0700

    Solutions to 21-24

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -22,11 +22,29 @@ let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
 console.log(result3); // -1
 *******************************************************************************/
 
-let firstIndex = function() {
-
+let firstIndex = function(array, cb) {
+    for (let item in array) {
+        if (cb(array[item])) {
+            return item
+        }
+    }
+    return -1
 };
 
+let result1 = firstIndex([3, 7, 8, 10], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1); // 2
 
+let result2 = firstIndex(['dog', 'cat', 'tree'], function (s) {
+    return s.length === 3;
+});
+console.log(result2); // 0
+
+let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
+    return s.length === 3;
+});
+console.log(result3); // -1
 
 
 
